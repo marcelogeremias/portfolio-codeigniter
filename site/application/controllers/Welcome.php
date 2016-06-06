205f1b2e1096ccb263b18e3c05e34dcef021d568
Marcelo Geremias <marcelo@marcelogeremias.com.br>
2016-06-05 21:30:37 -0300

    Correção da chamada da classe "MY_Controller"

--- site/application/controllers/Welcome.php
+++ site/application/controllers/Welcome.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 require_once 'My_Controller.php';
 
-class Welcome extends My_Controller {
+class Welcome extends MY_Controller {
 
 	public function index(){
 		echo "Ol&aacute; Pessoal.<br> Sejam benvidos &agrave; discplina LPII";
