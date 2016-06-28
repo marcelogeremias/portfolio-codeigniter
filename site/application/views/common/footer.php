c4c2d017da4ffd8ac34372a70fa780a203fcef70
Marcelo Geremias <marcelo@marcelogeremias.com.br>
2016-06-28 01:34:35 -0300

    Implementação do plugin de comentários do Facebook

--- site/application/views/common/footer.php
+++ site/application/views/common/footer.php
@@ -1,4 +1,10 @@
-
+		<section class="comentarios-pagina">
+			<div class="container">
+				<div class="row">
+					<div class="fb-comments" data-href="<?php echo base_url(uri_string()); ?>" data-width="100%" data-numposts="5"></div>
+				</div>
+			</div>
+		</section>
 		<footer>
 			<div class="container">
 				<div class="row">
